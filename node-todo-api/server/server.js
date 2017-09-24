3b8f1532025823bf8cf920365019b73f44fbedad
Xiner Zhang <szxiner@149-161-163-125.dhcp-bl.indiana.edu>
2017-09-24 18:42:44 -0400

    Todo app get by id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-//JSON to object
 const bodyParser = require('body-parser');
-
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -22,6 +20,31 @@ app.post('/todos', (req, res) => {
   })
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e);
+  })
+})
+
+app.get('/todos/:id',(req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
